e121ec32ded26a4e572c1d9755964ff32c552e42
Cristian Mircea Messel <mess110@gmail.com>
2010-08-04 00:17:56 +0300

    fixed password complexity

--- detection-base/app/models/user.rb
+++ detection-base/app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email, :message => "email already exists"
   validates_format_of :email, :with => /^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$/,
     :message => "invalid format"
-  validates_format_of :pass, :with => /^[a-zA-Z0-9-_!@#$%^&*(){}\[\]|.,<>:;"']{4,32}$/,
+    #TODO fix this crap
+  validates_format_of :pass, :with => /^[a-zA-Z0-9!@$#%^&*,.:;'"\|\-_ ]{4,32}$/,
     :message => "invalid. Between 4 and 32 characters, lower upper case letters and numbers"
 
   def before_save
