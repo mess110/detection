aaec5bb4acc8b94741635b45b8b420745a43f9af
Cristian Mircea Messel <mess110@gmail.com>
2010-10-17 13:05:37 +0200

    adding encrypt gem dependency
    find the gem in detection-gem encrypt

--- detection-base/config/environment.rb
+++ detection-base/config/environment.rb
@@ -17,6 +17,7 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem "cvwrapper", :version => '0.0.1'
+  config.gem "encrypt",   :version => '0.0.1'
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
