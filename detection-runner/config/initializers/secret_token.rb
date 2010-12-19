ab0c9af5f8b36deb44a3e9f7c7cecbd7ac1836cb
Cristian Mircea Messel <mess110@gmail.com>
2010-12-19 18:57:53 +0100

    initial commit for detection-runner
    The detection runner is what will do the heavy lifting. Detecting
    faces in an image.

---
+++ detection-runner/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DetectionRunner::Application.config.secret_token = 'fdf1aa9bdb5ee3febe237b24c00483752ebe95243a09f5701576d8bdd6e7f9bd40ea25583a97d0978668aceb2623245fe7fbc62518bae909ca01228879636c0e'
