c6157f50ed8d6c315e602ab2623093c3bab6e67d
Cristian Mircea Messel <mess110@gmail.com>
2011-01-02 17:01:12 +0200

    ubuntu setup script

---
+++ ubuntu_setup.sh
@@ -0,0 +1,30 @@
+DETECTION_DIRECTORY=$(cd `dirname $0` && pwd)
+
+# installing dependecies
+sudo apt-get install git subversion cmake build-essential
+
+# setting up opencv
+cd ~/Public
+mkdir opencv
+cd opencv
+svn co https://opencvlibrary.svn.sourceforge.net/svnroot/opencvlibrary/tags/latest_tested_snapshot
+cd latest_tested_snapshot/opencv
+mkdir release
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make
+sudo make install
+sudo ldconfig -v
+
+# installing light plugin
+sudo gem install jeweler
+cd $DETECTION_DIRECTORY
+cd detection-gems/light-opencv-wrapper
+sh install.sh
+
+# installing runner
+sudo apt-get install postgresql-8.4 pgadmin3 libpq-dev
+
+sudo gem install bundler
+cd $DETECTION_DIRECTORY
+cd detection-runner
+sudo bundle install
