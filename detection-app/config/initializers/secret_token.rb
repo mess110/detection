2322708949e23e03f4631f1d2398b8696cf74399
Cristian Mircea Messel <mess110@gmail.com>
2011-01-03 09:33:48 +0200

    add scheduling/api application

---
+++ detection-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DetectionApp::Application.config.secret_token = 'fa40851e147e6caddeeec8720828e90ea2beb1d7e2f3b927e6e77e5e8d00ef1c304c2781b3b90f804f25132e550ed8d740dbf6b214cc2fa1f27891cf7ab1839d'
