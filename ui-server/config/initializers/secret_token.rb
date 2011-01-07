3dc0a9703b04952e1de1e1c3e4fd7c7839e5fdd0
Cristian Mircea Messel <mess110@gmail.com>
2011-01-07 19:21:35 +0100

    add ui-server
    it will handle scheduling jobs, runners, the database and the api

---
+++ ui-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UiServer::Application.config.secret_token = '878beb8fb780509b37cb2545d8049661a836ba4207b71b6b2204bb1bf4e5ceae9f71e8c1054ef38451dbd373cdc227b38698c4f9924f1c51ce76cd8c645864af'
