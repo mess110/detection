c02e7ac0e0ced0a1e64a31ddc4f95a4b65358ff8
Cristian Mircea Messel <mess110@gmail.com>
2011-01-08 22:06:41 +0100

    draw image on canvas

--- ui-server/public/javascripts/application.js
+++ ui-server/public/javascripts/application.js
@@ -6,5 +6,29 @@ function clearInput(element) {
 }
 
 function detect() {
-  alert("detecting");
+  var url = "/api/v2/detect/new?url=" + $("urlbox").value;
+  new Ajax.Request(url, {
+    method:'get',
+    onSuccess: function(transport){
+      var response = transport.responseText;
+      var canvas = document.getElementById("detection");
+      var context = canvas.getContext("2d");
+      clearContext(context, canvas.width, canvas.height);
+      var img = new Image();
+      img.src = $("urlbox").value;
+      img.onload = function() {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+      }
+    },
+    onFailure: function(){
+      alert("crap");
+    }
+  });
+}
+
+function clearContext(context, width, height) {
+  context.fillStyle = "#FFFFFF";
+  context.fillRect(0, 0, width, height);
 }
