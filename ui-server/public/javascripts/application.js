872e4d57d5761cc6c5456bb93ad845b3f018c6f3
Cristian Mircea Messel <mess110@gmail.com>
2011-01-08 18:31:58 +0100

    javascript detect

--- ui-server/public/javascripts/application.js
+++ ui-server/public/javascripts/application.js
@@ -1,8 +1,10 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 function clearInput(element) {
   content = element.value;
   if ((content.substring(0,4) != 'http') && (content.substring(0,3) != 'ftp')) {
     element.value = '';
   }
+}
+
+function detect() {
+  alert("detecting");
 }
