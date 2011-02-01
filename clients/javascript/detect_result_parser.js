f406f6e6e76f038579968a95efb0dface96fd046
Cristian Mircea Messel <mess110@gmail.com>
2011-02-01 19:45:53 +0100

    update setup script

---
+++ clients/javascript/detect_result_parser.js
@@ -0,0 +1,57 @@
+function ApiError(code, description) {
+  this.code = code;
+  this.description = description;
+}
+
+function Region(tlx, tly, brx, bry) {
+  this.top_left_x = tlx;
+  this.top_left_y = tly;
+  this.bottom_right_x = brx;
+  this.bottom_right_y = bry;
+}
+
+function DetectResultParser(req) {
+  this.req = req;
+  
+  this.getRegions = function() {
+    var result = new Array();
+    var myregions = xmlDoc.getElementsByTagName("region");
+    for ( i = 0; i < myregions.length; i = i + 1)
+    {
+      region = myregions[i];
+      tlx = parseFloat(region.getAttribute("top_left_x"));
+      tly = parseFloat(region.getAttribute("top_left_y"));
+      brx = parseFloat(region.getAttribute("bottom_right_x"));
+      bry = parseFloat(region.getAttribute("bottom_right_y"));
+      result[i] = new Region(tlx, tly, brx, bry);
+    }
+    return result;
+  };
+
+  this.isCompleted = function() {
+    status = xmlDoc.getElementsByTagName("status")[0].childNodes[0].nodeValue;
+    return status == "completed";
+  };
+  
+  this.getError = function() {
+    code = xmlDoc.getElementsByTagName("code")[0].childNodes[0].nodeValue;
+    description = xmlDoc.getElementsByTagName("description")[0].childNodes[0].nodeValue;
+    return new ApiError(code, description);
+  }
+  
+  this.isError = function() {
+    return xmlDoc.getElementsByTagName("error")[0] != null;
+  };
+
+  if (window.DOMParser)
+  {
+    parser=new DOMParser();
+    xmlDoc=parser.parseFromString(req,"text/xml");
+  }
+  else // Internet Explorer
+  {
+    xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+    xmlDoc.async="false";
+    xmlDoc.loadXML(req);
+  }
+}
--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,12 +1,13 @@
 DETECTION_DIRECTORY=$(cd `dirname $0` && pwd)
 
 # installing dependecies
-sudo apt-get update; apt-get upgrade; apt-get install sudo git-core subversion cmake build-essential libgtk2.0-dev libjpeg-dev pkg-config postgresql libpq-dev libopenssl-ruby;
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git-core subversion cmake build-essential libgtk2.0-dev libjpeg-dev pkg-config postgresql libpq-dev libopenssl-ruby
 
 # setting up opencv
-cd ~
-mkdir opencv
-cd opencv
+mkdir ~/opencv
+cd ~/opencv
 svn co https://code.ros.org/svn/opencv/tags/latest_tested_snapshot
 cd latest_tested_snapshot/opencv
 mkdir release
@@ -14,14 +15,18 @@ cd release
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make
 sudo make install
-sudo ldconfig -v
+sudo ldconfig
 
 # installing light plugin
-sudo gem install jeweler
+sudo gem install jeweler rake
 cd $DETECTION_DIRECTORY
 cd spoc
-sh install.sh
+sudo sh install.sh
 
 cd $DETECTION_DIRECTORY
 cd runner
+sudo bundle install
+
+cd $DETECTION_DIRECTORY
+cd ui-server
 sudo bundle install
