887634f592f2939c1519c4de9d56ef040dcca4fd
Cristian Mircea Messel <mess110@gmail.com>
2011-02-02 11:41:22 +0100

    upload from file post request

--- ui-server/app/controllers/api/v2/detect_controller.rb
+++ ui-server/app/controllers/api/v2/detect_controller.rb
@@ -1,5 +1,8 @@
 #http://sphotos.ak.fbcdn.net/photos-ak-snc1/v272/12/29/1352704123/n1352704123_30009292_5638.jpg
 class Api::V2::DetectController < ApplicationController
+
+  protect_from_forgery :except => :new
+
   def new
     if request.get?
       if params[:url].nil?
@@ -27,8 +30,18 @@ class Api::V2::DetectController < ApplicationController
 
       render :partial => "new" and return
     elsif request.post?
-      # uploading files will be handled with post request
+      # TODO: remove this when implemented
       render_api_error('invalid_request', 'not yet implemented') and return
+      
+      if params[:file].nil?
+        render_api_error('invalid_url', 'file parameter required') and return
+      end
+      
+      filename =  params[:file].original_filename
+      path = File.join("public/data", filename)
+      file_content = params[:file].read
+      File.open(path, "wb") { |f| f.write(file_content) }
+      render_api_error('invalid_request', "console output") and return
     else
       render_api_error('invalid_request', 'put and delete now allowed') and return
     end
